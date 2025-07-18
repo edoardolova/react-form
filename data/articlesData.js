b4c3be445a05d8e076f2e96023e2798c093646e3
Edoardo Lova <edoardolova81@gmail.com>
2025-07-18 12:38:03 +0200

    add articlesData, form for articles with input validation and dinamic articles list

---
+++ data/articlesData.js
@@ -0,0 +1,9 @@
+const articlesData = [
+  { title: "Come iniziare con JavaScript" },
+  { title: "Introduzione a React" },
+  { title: "Guida a Node.js per principianti" },
+  { title: "10 trucchi per migliorare il tuo codice" },
+  { title: "Cos'è l'Asynchronous JavaScript" }
+];
+
+export default articlesData;
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
 import { useState } from 'react'
 import './App.css'
+import articlesData from '../data/articlesData'
 
 function App() {
+  const articlesArr = articlesData;
+  const [newArticle, setNewArticle] = useState('');
+  const [articles, setArticles] = useState(articlesArr)
 
+  function handleSubmit(e){
+    e.preventDefault();
+    if (!newArticle.trim()) {
+      return;
+    }
+    const newArticleObj = {title: newArticle};
+    const updatedArticles = [...articles, newArticleObj];
+    setArticles(updatedArticles);
+    setNewArticle('');
+  }
   return (
     <>
+      <div className="container">
+        <div className="header py-3">
+          <h1>Benvenuti sul nosto blog</h1>
+        </div>
+        <ul className='list-group mb-5'>
+          {articles.map((article, index)=>{
+            return(
+              <li className='list-group-item' key={index}>{article.title}</li>
+            )
+          })}
+        </ul>
+        <form onSubmit={handleSubmit}>
+          <div className='d-flex'>
+            <input className='form-control' type="text" value={newArticle} 
+            onChange={e => {setNewArticle(e.target.value)}}/>
+            <button type='submit' className='btn btn-success ms-3'>AGGIUNGI</button>
+          </div>
+        </form>
+      </div>
 
     </>
   )
